8b0a58731e460926bb26e1feb4223120e26df2c6
Renjith-1 <jetmax543@gmail.com>
2026-04-22 11:28:41 +0530

    feat: Implemented basic endpoints

---
+++ src/main/java/com/rp/quotes/controller/QuotesController.java
@@ -0,0 +1,21 @@
+package com.rp.quotes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quotes")
+public class QuotesController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@GetMapping("/up")
+	public String up() {
+		return "App is Up";
+	}
+
+}
